96c2caf7780cde30bb611f44c54fffd34b769231
Corey Lambert <157275713+CsTL0522@users.noreply.github.com>
2025-02-03 04:49:50 -0600

    more steps

--- app.js
+++ app.js
@@ -1,29 +1,61 @@
 const { render } = require('ejs');
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose')
+const Blog = require('./models/blog');
+const BlogRoutes = require('./routes/blogRoutes');
 
 //express app
 
 const app = express();
 
-
+//connect to mongodb
+const dbURI = 'mongodb+srv://ninja:powpow@nodetutorial.pi758.mongodb.net/?retryWrites=true&w=majority&appName=nodeTutorial'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 // register view engine
 app.set('view engine', 'ejs');
 
 // listen for requests
-app.listen(3000);
+
 
 //middleware staTic files
 
 app.use(express.static('public'));
 
 app.use(morgan('dev'));
-  
+
 app.use((req, res, next) => {
     res.locals.path = req.path;
     next();
   });
   
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog',
+        snppet: 'about my new blog',
+        body: 'more about my blog'
+    });
+
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+  
+app.get('/all-blogs' , (req, res) => {
+    Blog.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+})
 
 // app.set('views','myviews');
 
